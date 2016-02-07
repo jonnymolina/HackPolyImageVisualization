d11d516584091127d95bfb4d5bc0ab52f8762634
Rushil Patel <rushil9@gmail.com>
2016-02-07 01:21:19 -0800

    finished functions.py

--- Visualize/functions.py
+++ Visualize/functions.py
@@ -1,6 +1,8 @@
 from clarifai.client import ClarifaiApi
-import urllib.request
-
+import requests
+from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
+import json
 class Functions:
 	
 	def __init__(self):
@@ -8,14 +10,39 @@ class Functions:
 		print("initialized")
 
 	def get_tags(self, url):
-		result = self.clarifai_api.tag_image_urls('http://www.gettyimages.ca/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg')
-		return result["results"][0]["result"]["tag"]["classes"]
+		result = self.clarifai_api.tag_image_urls(url)
+		tags = result["results"][0]["result"]["tag"]["classes"]
+		conf_intervals = result["results"][0]["result"]["tag"]["probs"]
 
+		return get_tag_sets(tags, conf_intervals)
 
-	def get_image(self, url):
-		pass
+	def get_tag_sets(tags, conf_intervals):
+
+		size = len([x for x in conf_intervals if x > 0.90])
+		tag_sets = []
+
+		for i in range(size - int(size/3)):
+			tag_sets.append((tuple(tags[i:i + int(size/3)])))
+
+		return tag_sets		
+
+	def get_image_url(self, tags):
+		url = "https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query=%27"+ "%20".join(tags) +"%27"
+		print(url)
+		authent = HTTPBasicAuth("rushil9","4MJB6yiLpF5H2qI+RTkLfomBbLHNYTVdfccpC73YlK0")
+		response = requests.get(url, auth = authent).text
+		response = eval(response)
+		targ_url = response["d"]["results"][0]["MediaUrl"]
+		return targ_url
 
 	def load(self, url):
-		pass
+		tag_sets = get_tags(self, url)
+		image_urls = []
+		for tag in tag_sets:
+			image_urls.append(get_image_url(tag))
 
-Functions().get_tags("ddd")
+		return image_urls
+		
+
+#print(Functions().get_tags("http://dreamatico.com/data_images/animals/animals-4.jpg"))
+print(Functions().get_image_url( tags=["fuzzy", "cats", "cute"]))
